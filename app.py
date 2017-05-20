b4eb7e3041da7e64c7a7f30e4058732371de0c0e
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2017-05-20 20:08:07 +0530

    corrected mistakeS

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def webhook():
         payload = request.get_data()
         sender, message = messaging_events(payload)
         user = db.user.find_one({"fbId": sender})
-        if complaint(payload):
+        try:
             complaint_text = complaint(payload)
             print complaint_text
+        except:
+            pass
         fbinfo = get_user_info(sender)
         if user is None:
             db.user.insert({"fbId": sender ,  "first_name" : fbinfo["first_name"] , "last_name" : fbinfo["last_name"] , "profile_pic" : fbinfo["profile_pic"]})
