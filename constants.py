b6c18b80b1d4b02c7c31a116c6d85216d758edcb
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2017-05-20 19:59:51 +0530

    corrected mistakeS

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import urllib 
 
 VERIFY_TOKEN = 'ah10'
-PAT = 'EAAScnE8mOAoBALZBYAOwds2ylqgr5nGu29nhth0UyvUlm84TC7qYUWxdq8KbAnLYwaM7W2ZAXKcf2pRBeD0cxL4iDQrRlc8sa1q3nkASfJa2Btl7rNBv4oDdofNV7YD7gxXmqwn78xabotjFZCIHOb1J03qb6fMgOcVCZCMC9wZDZD'
+PAT = 'EAAScnE8mOAoBAJwsCpRtVoBWBkzZAu15PEjFetLkFl5a3bHRF7QVyChNnJHFU7rfci61DaWO03Chh5z2cL9ZBvSmHqbGJ2n7zilHqskWeY8xwOV5yG8iveDdd9EAInYSbHExxpuAsYEB7rJJoyx2QCwsWMHko3NqFK8tYbaQZDZD'
 SEND_MESSAGE_URL = 'https://graph.facebook.com/v2.6/me/messages'
 THREAD_URL = 'https://graph.facebook.com/v2.6/me/thread_settings'
 CONNECTION = 'mongodb://vaibhavgeek:nonsense@ds147681.mlab.com:47681/angelhack10'
